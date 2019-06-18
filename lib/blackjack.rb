22e8c785f40183fb3eeaa40799d6e48b78a08ff4
Daniel Voigt <danielhvoigt@gmail.com>
2019-06-18 21:22:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,6 +50,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  hand = initial_round
+  until hand > 21 do
+    hand = hit?(hand)
+    display_card_total(hand)
+  end
+  end_game(hand)
 end
     
